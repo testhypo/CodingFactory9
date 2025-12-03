a8d075562039a8175085b8d736fbb69740939056
testhype <chris676@gmail.com>
2025-12-03 20:58:09 +0200

    feat: add immutable class Point

---
+++ src/gr/aueb/cf/cf9/ch11/Point.java
@@ -0,0 +1,37 @@
+package gr.aueb.cf.cf9.ch11;
+
+/**
+ * Immutable class
+ * - fields are final
+ * - no setters
+ */
+public class Point {
+    private final int x;
+    private final int y;
+
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+//    public void setX(int x) {
+//        this.x = x;
+//    }
+
+    public int getY() {
+        return y;
+    }
+
+//    public void setY(int y) {
+//        this.y = y;
+//    }
+}
