d3a7e4e53aad399bc64cf89bea9c3159e59ff725
testhype <chris676@gmail.com>
2025-12-08 19:51:57 +0200

    fix: add private HelloUtil statement

--- src/gr/aueb/cf/cf9/ch13/HelloUtil.java
+++ src/gr/aueb/cf/cf9/ch13/HelloUtil.java
@@ -9,6 +9,8 @@ public class HelloUtil {
     /**
      * No instances of this class should be available.
      */
+    private HelloUtil() {}
+
     public static void sayHello() {
         System.out.println("Hello Coding!");
     }
--- src/gr/aueb/cf/cf9/ch13/Main.java
+++ src/gr/aueb/cf/cf9/ch13/Main.java
@@ -2,6 +2,7 @@ package gr.aueb.cf.cf9.ch13;
 
 public class Main {
 
+
     public static void main(String[] args) {
         HelloUtil.sayHello();
     }
