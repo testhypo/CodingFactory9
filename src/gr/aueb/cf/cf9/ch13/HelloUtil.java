cd916c051924cb3a55d5b5974e5574c62f91307d
testhype <chris676@gmail.com>
2025-12-08 19:49:06 +0200

    feat: helloutil utility class and main

---
+++ src/gr/aueb/cf/cf9/ch13/HelloUtil.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.cf9.ch13;
+
+/**
+ * Utility classes are non-instantiable.
+ * They provide public static methods only.
+ */
+public class HelloUtil {
+
+    /**
+     * No instances of this class should be available.
+     */
+    public static void sayHello() {
+        System.out.println("Hello Coding!");
+    }
+}
