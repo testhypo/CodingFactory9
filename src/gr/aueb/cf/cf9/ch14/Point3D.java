e9a334099fbde657a4511f2259f091325f375568
testhype <chris676@gmail.com>
2025-12-09 19:17:01 +0200

    feat: add Point3D subclass

--- src/gr/aueb/cf/cf9/ch14/Point3D.java
+++ src/gr/aueb/cf/cf9/ch14/Point3D.java
@@ -1,4 +1,35 @@
 package gr.aueb.cf.cf9.ch14;
 
-public class Point3D {
+public class Point3D extends Point2D{
+    private double z;
+
+    public Point3D() {
+//        super(); // constructor chaining
+//        z = 0.0;
+    }
+
+    public Point3D(double x, double y, double z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    @Override
+    public void movePlusOne() {
+        super.movePlusOne();
+        z += 1;
+    }
+
+    @Override
+    protected void movePlus10() {
+        super.movePlus10();
+        z += 10;
+    }
 }
