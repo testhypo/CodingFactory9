f3fec8a196b11510aa07913d496cb1b67bd2606b
testhype <chris676@gmail.com>
2025-12-09 18:48:04 +0200

    feat: add point class

---
+++ src/gr/aueb/cf/cf9/ch14/Point.java
@@ -0,0 +1,33 @@
+package gr.aueb.cf.cf9.ch14;
+
+public class Point {
+    private double x;
+
+    public Point() {
+
+    }
+
+    public Point(double x) {
+        this.x = x;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void movePlusOne() {
+        x++;
+    }
+
+    protected void movePlus10() {
+        x += 10;
+    }
+
+    private void reset() {
+        x = 0;
+    }
+}
