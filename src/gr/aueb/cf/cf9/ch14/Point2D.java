7ad53337eebe254759333d16604ce140f249ae3e
testhype <chris676@gmail.com>
2025-12-09 19:13:50 +0200

    fix: getter and setter to Point2D

--- src/gr/aueb/cf/cf9/ch14/Point2D.java
+++ src/gr/aueb/cf/cf9/ch14/Point2D.java
@@ -1,4 +1,35 @@
 package gr.aueb.cf.cf9.ch14;
 
-public class Point2D {
+public class Point2D extends Point {
+    private double y;
+
+    public Point2D() {
+        // super();
+        // y = 0.0;
+    }
+
+    public Point2D(double x, double y) {
+        super(x);
+        this.y = y;
+    }
+
+    @Override
+    public void movePlusOne() {
+        super.movePlusOne();
+        y++;
+    }
+
+    @Override
+    protected void movePlus10() {
+        super.movePlus10();
+        y += 10;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
 }
