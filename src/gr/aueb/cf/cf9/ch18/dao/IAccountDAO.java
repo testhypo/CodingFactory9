2134dc7d576914b44f3a7ea0f4d56e39c3d32efb
testhype <chris676@gmail.com>
2025-12-23 19:39:53 +0200

    feat: add Account CRUD Contract (interface)

---
+++ src/gr/aueb/cf/cf9/ch18/dao/IAccountDAO.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.cf9.ch18.dao;
+
+import gr.aueb.cf.cf9.ch18.model.Account;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IAccountDAO {
+    Account saveOrUpdate(Account account);
+    void remove(String iban);
+    Optional<Account> findByIban(String iban);
+    List<Account> getAllAccounts();
+    // Queries
+}
