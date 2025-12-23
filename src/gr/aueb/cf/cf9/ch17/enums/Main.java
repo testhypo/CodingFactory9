3827ddc8bbfdc05dc1e3553d99c09ad4afc49317
testhype <chris676@gmail.com>
2025-12-23 19:26:12 +0200

    feat: add enum logic

---
+++ src/gr/aueb/cf/cf9/ch17/enums/Main.java
@@ -0,0 +1,20 @@
+package gr.aueb.cf.cf9.ch17.enums;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserRole role = UserRole.ADMIN;
+
+        String roleAdmin = role.name();     // ADMIN
+        System.out.println("role: " + roleAdmin + ", ordinal: " + role.ordinal());
+
+        UserRole roleUser = UserRole.valueOf(roleAdmin);
+
+        for (UserRole r : UserRole.values()) {
+            System.out.println(r.ordinal() + ", " + r.name());
+        }
+
+    }
+}
---
+++ src/gr/aueb/cf/cf9/ch17/enums/UserRole.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf.cf9.ch17.enums;
+
+public enum UserRole {
+    ADMIN,
+    TEACHER,
+    STUDENT
+}
