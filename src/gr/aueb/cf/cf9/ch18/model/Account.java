5c20a440120905d6091e331ff0508219f6fad694
testhype <chris676@gmail.com>
2025-12-23 19:27:09 +0200

    feat: add Account model entities

---
+++ src/gr/aueb/cf/cf9/ch18/model/Account.java
@@ -0,0 +1,53 @@
+package gr.aueb.cf.cf9.ch18.model;
+
+import java.util.Objects;
+
+public class Account {
+    private String iban;
+    private double balance;
+
+    public Account() {
+
+    }
+
+    public Account(String iban, double balance) {
+        this.iban = iban;
+        this.balance = balance;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "iban='" + iban + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Account account)) return false;
+        return Objects.equals(this.getIban(), account.getIban());
+        }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getIban(), getBalance());
+    }
+}
